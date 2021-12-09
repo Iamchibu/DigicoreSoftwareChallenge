b9dd32d7a7eeb61370bf7330eafb76ba695d5d0b
Chibundom Ejimuda <chibundomejimuda@gmail.com>
2021-12-09 11:55:58 +0100

    Question3

---
+++ Question3.js
@@ -0,0 +1,16 @@
+function kangarooCount(a,b,v1,v2){
+	var k1 = a * v1
+	var k2 = b * v2
+	var result1 = 'YES';
+	var result2 = 'NO';
+	if (k1 = k2){
+	  console.log(result1)
+    return result1;
+	} 
+	else{
+	  console.log(result2)
+	return result2;
+	}
+}
+
+kangarooCount(1,2,2,1);
